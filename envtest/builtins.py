9d93a9d2fc5bb75b05649313fccfb8855cca011f
acse-cxd20 <calvinxdavies@icloud.com>
2020-10-14 11:11:44 +0100

    Commiting new changes, added new functions

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -8,8 +8,9 @@ __all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
 def rand_array(shape):
     return np.random.rand(*shape)
 
-def smooth_image(a, sigma=1):
-    return gaussian_filter(a, sigma=sigma)
+def smooth_image(a, sigma = 1):
+    return gaussian_filter(a,sigma=sigma)
 
-def my_mat_solve(A, b):
-    return A.inv()*b
+def my_mat_solve(A,b):
+	return A.inv()*b
+
